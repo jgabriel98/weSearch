a71218a9f73b1840f84d94209a89492fcdf7890f
João Gabriel <jgabsfernandes@gmail.com>
2020-01-23 04:38:13 -0300

    import new fonts

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import './fonts/merriweather/Merriweather-Regular.ttf';
+import './fonts/merriweather/Merriweather-Bold.ttf';
+import './fonts/merriweather/Merriweather-Italic.ttf';
+import './fonts/merriweather/Merriweather-Light.ttf';
+import './fonts/merriweather/Merriweather-BoldItalic.ttf';
+import './fonts/merriweather/Merriweather-LightItalic.ttf';
+
+import './fonts/sourceSansPro/SourceSansPro-Italic.ttf';
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
