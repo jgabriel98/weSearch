d7d34c24489f224d3c553b8d275a3c7ff4d8e394
João Gabriel <jgabsfernandes@gmail.com>
2020-01-28 01:58:50 -0300

    estrutura do PostBox, presente no resultado de pesquisa

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -25,10 +25,54 @@ function AbasSearchbar() {
     )
 }
 
-function ResultadoSearchbar(){
+function ResultadoSearchbar() {
+    const imgSize = 226
+    const largura = 306
+    const alturaBase = 293, alturaFinal = 249
+    const maxStep = 0.914675768 //um passo n pode diminuir mais que isso (valor em porcentagem)
+    const minStep = 1.176706827 //um passo n pode aumentar mais que isso (valor em porcentagem)
+
+    let range = [1, 2, 3]
+
+    // seja n == range.size(), e X==step, queremos:  alturaBase*(X^n) = alturaFinal
+    // então: x = raiz-n de (alturaFinal/alturaBase)
+    let step = Math.pow(alturaFinal / alturaBase, 1 / range.length)
+    step = Math.min(Math.max(step, maxStep), minStep)
+
+
+    //trabalho em progresso, ta mal organizado mesmo
+    const boxes = range.map((value, i) => {
+        return (
+            <div className='PostBox'>
+                <div style={{
+                    backgroundColor: 'green',
+                    height: imgSize
+                }}></div>
+
+                <div className='PostBox_down' style={{
+                    height: alturaBase * Math.pow(step, i + 1),
+                    backgroundColor: 'white'
+                }}>
+
+                    <div className='PostBox_titulo'>
+                        <span>Melanoma</span>
+                    </div>
+                    <ul className='PostBox_conteudo'>
+                        <li className='PostBox_autor'></li>
+                        <li className='PostBox_descricao'></li>
+                        <li className='PostBox_tags'></li>
+                        <li className='PostBox_interagirBtn'></li>
+                    </ul>
+
+                </div>
+            </div>
+        )
+    })
 
     return (
-        <div></div>
+        <div style={{ display: 'inline-flex' }}>
+            {boxes}
+        </div>
     )
 }
 
@@ -38,6 +82,8 @@ class Main extends Component {
             //<CarouselFeed className="CarouselFeed" />
             <div className="MainPageContainer">
                 <CarouselSlider className="Slider" />
+
+                {/*TODO: reorganizar esses tres abaixo para um componente só em outro arquivo*/}
                 <Searchbar />
                 <AbasSearchbar />
                 <ResultadoSearchbar />
